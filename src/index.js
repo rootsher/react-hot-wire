653d5150055ca3d59b77a5991b3beaac1447eb95
Konrad Kowalski <rootsher@gmail.com>
2019-11-19 14:08:02 +0100

    Refactor usage example - move class to plain function

--- src/index.js
+++ src/index.js
@@ -1,58 +1,56 @@
 import React from 'react';
 
 export const createInstance = () => {
-	const Context = React.createContext({});
+    const Context = React.createContext({});
 
-	function find(dependencies, services) {
-		return dependencies.reduce((result, dependency) => {
-			result[dependency] = services[dependency];
+    function find(dependencies, services) {
+        return dependencies.reduce((result, dependency) => {
+            result[dependency] = services[dependency];
 
-			return result;
-		}, {});
-	}
+            return result;
+        }, {});
+    }
 
-	function Provider({ services, children }) {
-		return <Context.Provider value={services}>{children}</Context.Provider>;
-	}
+    function Provider({ services, children }) {
+        return <Context.Provider value={services}>{children}</Context.Provider>;
+    }
 
-	function Wire({ services }) {
-		return (
-			<Context.Consumer> {_services => this.props.render(find(services, _services))}</Context.Consumer>
-		);
-	}
+    function Wire({ services }) {
+        return <Context.Consumer>{_services => this.props.render(find(services, _services))}</Context.Consumer>;
+    }
 
-	function wire(dependencies, Component) {
-		return props => (
-			<Context.Consumer>
-				{services => <Component {...find(dependencies, services)} {...props} />}
-			</Context.Consumer>
-		);
-	}
+    function wire(dependencies, Component) {
+        return props => (
+            <Context.Consumer>
+                {services => <Component {...find(dependencies, services)} {...props} />}
+            </Context.Consumer>
+        );
+    }
 
-	class Service {
-		_changeListeners = [];
+    class Service {
+        _changeListeners = [];
 
-		addChangeListener(changeListener) {
-			this._changeListeners.push(changeListener);
+        addChangeListener(changeListener) {
+            this._changeListeners.push(changeListener);
 
-			return () => this._unregisterListener(changeListener);
-		}
+            return () => this._unregisterListener(changeListener);
+        }
 
-		runChangeListeners() {
-			this._changeListeners.forEach(listener => listener());
-		}
+        runChangeListeners() {
+            this._changeListeners.forEach(listener => listener());
+        }
 
-		_unregisterListener(changeListener) {
-			this._changeListeners = this._changeListeners.filter(listener => listener !== changeListener);
-		}
-	}
+        _unregisterListener(changeListener) {
+            this._changeListeners = this._changeListeners.filter(listener => listener !== changeListener);
+        }
+    }
 
-	return {
-		Provider,
-		Wire,
-		wire,
-		Service,
-	};
+    return {
+        Provider,
+        Wire,
+        wire,
+        Service,
+    };
 };
 
 export const { Provider, Wire, wire, Service } = createInstance();
